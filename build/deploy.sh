cdf5892f61d0447ed0b3871ee7276e4eea710597
manikandanS45 <manikandanmaggi@gmail.com>
2025-05-12 22:35:14 +0530

    s

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,7 +7,7 @@ echo "🔐 Logging into Docker Hub..."
 echo "$DOCKER_CREDS_PSW" | docker login -u "$DOCKER_CREDS_USR" --password-stdin
 
 # Set image tags based on environment
-if [ "$ENV" = "prod" ]; then
+if [ "$ENV" = "prod" ]; then 
     TARGET_IMAGE="mani0045/prod:v1"   # Replace with actual repo
     docker tag webapp:v1 $TARGET_IMAGE
 else
